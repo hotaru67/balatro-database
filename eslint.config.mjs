9726255cf160b5d179accebe18ee4c51c0f0b4ab
Alex Du <du@Alexs-Air.lan>
2025-03-29 08:16:24 +1100

    finished database

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import json from "@eslint/json";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+        plugins: {
+            json,
+        },
+    },
+
+    // lint JSON files
+    {
+        files: ["**/*.json"],
+        language: "json/json",
+        rules: {
+            "json/no-duplicate-keys": "error",
+        },
+    },  
 ];
 
 export default eslintConfig;
